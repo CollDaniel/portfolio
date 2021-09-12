1544e49c252eb4bcbe796c179ef84d07e8bc74d1
DanielColl <daniel_cr27@hotmail.com>
2021-09-12 00:01:44 -0500

    SideBar Menu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SideBar from "./Components/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+    </div>
+  );
+}
+
+// const AppStyled = styled.div`
+//   background-color: red;
+// `;
+export default App;
